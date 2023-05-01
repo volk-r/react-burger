e2fa823bd457e64ca768322728bcb141c2e4d9c8
Roman Romanov <rromanov.vk@gmail.com>
2023-05-01 10:15:21 +0500

    layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import './app.css';
+import AppStyles from './app.module.css';
 
 import AppHeader from '../header/header'
+import BurgerIngredients from '../burger-ingredients/burger-ingredients'
+import BurgerConstructor from '../burger-constructor/burger-constructor'
+
+;
+// import BurgerIngredients from '@ya.praktikum/react-developer-burger-ui-components';
 
 class App extends React.Component {
     render() {
         return (
             <>
                 <AppHeader />
+                <main className={ AppStyles.box }>
+                    <BurgerIngredients />
+                    <BurgerConstructor />
+                </main>
             </>
         );
     }
---
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import BurgerIngredientsStyles from './burger-ingredients.module.css'
+
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
+
+import { data } from '../../utils/data';
+import BurgerConstructorStyles from "../burger-constructor/burger-constructor.module.css";
+
+export default function BurgerIngredients(props) {
+    const [current, setCurrent] = React.useState('one')
+
+    // state = {
+    //     current: 'sause',
+    //     data: data,
+    // };
+    // getTypes = () => {
+    //     return this.state.data.map(el => el.type)
+    // };
+
+    return (
+        <>
+            <section className={ BurgerIngredientsStyles.container }>
+                <p className="p-1 mt-10 mb-3 text text_type_main-large">
+                    Соберите бургер
+                </p>
+                <div style={{ display: 'flex' }}>
+                    <Tab value="sause" active={current === 'sause'} onClick={setCurrent}>
+                        Соусы
+                    </Tab>
+                    <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+                        Булки
+                    </Tab>
+                    <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+                        Начинка
+                    </Tab>
+                </div>
+                <p className="text text_type_main-medium mb-5">
+                </p>
+                <ul className={` ${ BurgerIngredientsStyles.listContainer } custom-scroll`}>
+                    <li>
+                        <p className="text text_type_main-medium">
+                            Булки
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+                        <p className="text text_type_main-large p-5">
+                            ingridients
+                        </p>
+
+                    </li>
+                </ul>
+            </section>
+        </>
+    );
+}
