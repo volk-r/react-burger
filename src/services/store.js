8ec8b4a8461de2446d75e56ddb9005555ee3c701
Roman Romanov <rromanov.vk@gmail.com>
2023-05-20 15:38:41 +0500

    prepare store

--- src/services/store.js
+++ src/services/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { rootReducer } from './reducers/index';
+import thunk from 'redux-thunk';
 
 const initialState = {
     ingredients: [],
@@ -9,6 +10,10 @@ const initialState = {
     order: null,
 };
 
-const store = createStore(rootReducer, initialState, composeWithDevTools());
+const composeEnhancers = composeWithDevTools({});
+
+const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(thunk),
+));
 
 export default store;
